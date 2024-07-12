c853ee3221fcbbe4f4a176ad9e229b31c6a6db2b
sliman4 <4sliman4@gmail.com>
2024-07-13 02:20:01 +0300

    Add contract indexer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use contract_indexer::txt_file_storage::TxtFileStorage;
+use contract_indexer::RPC_URL;
 use inindexer::multiindexer::{ChainIndexers, MapError};
 use inindexer::neardata_server::NeardataServerProvider;
 use inindexer::{
     run_indexer, AutoContinue, BlockIterator, IndexerOptions, PreprocessTransactionsSettings,
 };
+use near_jsonrpc_client::JsonRpcClient;
 use redis::aio::ConnectionManager;
 
 #[tokio::main]
@@ -29,10 +32,16 @@ async fn main() {
     let trade_indexer = trade_indexer::TradeIndexer(
         trade_indexer::redis_handler::PushToRedisStream::new(connection.clone(), 10_000).await,
     );
+    let contract_indexer = contract_indexer::ContractIndexer::new(
+        contract_indexer::redis_handler::PushToRedisStream::new(connection.clone(), 10_000).await,
+        JsonRpcClient::connect(std::env::var("RPC_URL").unwrap_or(RPC_URL.to_string())),
+        TxtFileStorage::new("known_tokens.txt").await,
+    );
     let mut indexer = nft_indexer
         .map_error(anyhow::Error::msg)
         .chain(potlock_indexer)
-        .chain(trade_indexer.map_error(anyhow::Error::msg));
+        .chain(trade_indexer.map_error(anyhow::Error::msg))
+        .chain(contract_indexer);
 
     run_indexer(
         &mut indexer,
