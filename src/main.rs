f6d5e11817ac0dd14c2a404e72870039bd4734ea
sliman4 <4sliman4@gmail.com>
2024-07-25 08:34:07 +0300

    Add log and socialdb indexers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use contract_indexer::txt_file_storage::TxtFileStorage;
 use contract_indexer::RPC_URL;
 use inindexer::multiindexer::{ChainIndexers, MapError};
+use inindexer::near_utils::TESTNET_GENESIS_BLOCK_HEIGHT;
 use inindexer::neardata_server::NeardataServerProvider;
 use inindexer::{
     run_indexer, AutoContinue, BlockIterator, IndexerOptions, PreprocessTransactionsSettings,
@@ -23,57 +24,121 @@ async fn main() {
     .unwrap();
     let connection = ConnectionManager::new(client).await.unwrap();
 
-    let nft_indexer = nft_indexer::NftIndexer(
-        nft_indexer::redis_handler::PushToRedisStream::new(connection.clone(), 10_000).await,
-    );
-    let potlock_indexer = potlock_indexer::PotlockIndexer(
-        potlock_indexer::redis_handler::PushToRedisStream::new(connection.clone(), 10_000).await,
-    );
-    let trade_indexer = trade_indexer::TradeIndexer(
-        trade_indexer::redis_handler::PushToRedisStream::new(connection.clone(), 10_000).await,
-    );
-    let contract_indexer = contract_indexer::ContractIndexer::new(
-        contract_indexer::redis_handler::PushToRedisStream::new(connection.clone(), 10_000).await,
-        JsonRpcClient::connect(std::env::var("RPC_URL").unwrap_or(RPC_URL.to_string())),
-        TxtFileStorage::new("known_tokens.txt").await,
-    );
-    let mut indexer = nft_indexer
-        .map_error(anyhow::Error::msg)
-        .chain(potlock_indexer)
-        .chain(trade_indexer.map_error(anyhow::Error::msg))
-        .chain(contract_indexer);
+    if std::env::var("TESTNET").is_ok() {
+        log::warn!("Running all-indexers on testnet");
 
-    run_indexer(
-        &mut indexer,
-        NeardataServerProvider::mainnet(),
-        IndexerOptions {
-            range: if std::env::args().len() > 1 {
-                // For debugging
-                let msg = "Usage: `all-indexers` or `all-indexers [start-block] [end-block]`";
-                BlockIterator::iterator(
-                    std::env::args()
-                        .nth(1)
-                        .expect(msg)
-                        .replace(['_', ',', ' ', '.'], "")
-                        .parse()
-                        .expect(msg)
-                        ..=std::env::args()
-                            .nth(2)
+        let log_indexer = log_indexer::LogIndexer(
+            log_indexer::redis_handler::PushToRedisStream::new(
+                connection.clone(),
+                100_000,
+                Some("testnet_".to_string()),
+            )
+            .await,
+        );
+        let mut indexer = log_indexer.map_error(anyhow::Error::msg);
+
+        run_indexer(
+            &mut indexer,
+            NeardataServerProvider::testnet(),
+            IndexerOptions {
+                range: if std::env::args().len() > 1 {
+                    // For debugging
+                    let msg = "Usage: `all-indexers` or `all-indexers [start-block] [end-block]`";
+                    BlockIterator::iterator(
+                        std::env::args()
+                            .nth(1)
                             .expect(msg)
                             .replace(['_', ',', ' ', '.'], "")
                             .parse()
-                            .expect(msg),
-                )
-            } else {
-                BlockIterator::AutoContinue(AutoContinue::default())
+                            .expect(msg)
+                            ..=std::env::args()
+                                .nth(2)
+                                .expect(msg)
+                                .replace(['_', ',', ' ', '.'], "")
+                                .parse()
+                                .expect(msg),
+                    )
+                } else {
+                    BlockIterator::AutoContinue(AutoContinue {
+                        save_location: Box::new("last-processed-block-testnet.txt"),
+                        start_height_if_does_not_exist: TESTNET_GENESIS_BLOCK_HEIGHT,
+                        ..Default::default()
+                    })
+                },
+                preprocess_transactions: Some(PreprocessTransactionsSettings {
+                    prefetch_blocks: if cfg!(debug_assertions) { 0 } else { 100 },
+                    postfetch_blocks: if cfg!(debug_assertions) { 0 } else { 100 },
+                }),
+                ..Default::default()
             },
-            preprocess_transactions: Some(PreprocessTransactionsSettings {
-                prefetch_blocks: if cfg!(debug_assertions) { 0 } else { 100 },
-                postfetch_blocks: if cfg!(debug_assertions) { 0 } else { 100 },
-            }),
-            ..Default::default()
-        },
-    )
-    .await
-    .expect("Indexer run failed");
+        )
+        .await
+        .expect("Indexer run failed");
+    } else {
+        let nft_indexer = nft_indexer::NftIndexer(
+            nft_indexer::redis_handler::PushToRedisStream::new(connection.clone(), 10_000).await,
+        );
+        let potlock_indexer = potlock_indexer::PotlockIndexer(
+            potlock_indexer::redis_handler::PushToRedisStream::new(connection.clone(), 10_000)
+                .await,
+        );
+        let trade_indexer = trade_indexer::TradeIndexer(
+            trade_indexer::redis_handler::PushToRedisStream::new(connection.clone(), 10_000).await,
+        );
+        let contract_indexer = contract_indexer::ContractIndexer::new(
+            contract_indexer::redis_handler::PushToRedisStream::new(connection.clone(), 10_000)
+                .await,
+            JsonRpcClient::connect(std::env::var("RPC_URL").unwrap_or(RPC_URL.to_string())),
+            TxtFileStorage::new("known_tokens.txt").await,
+        );
+        let socialdb_indexer = socialdb_indexer::SocialDBIndexer(
+            socialdb_indexer::redis_handler::PushToRedisStream::new(connection.clone(), 10_000)
+                .await,
+        );
+        let log_indexer = log_indexer::LogIndexer(
+            log_indexer::redis_handler::PushToRedisStream::new(connection.clone(), 100_000, None)
+                .await,
+        );
+        let mut indexer = nft_indexer
+            .map_error(anyhow::Error::msg)
+            .chain(potlock_indexer)
+            .chain(trade_indexer.map_error(anyhow::Error::msg))
+            .chain(contract_indexer)
+            .chain(socialdb_indexer.map_error(anyhow::Error::msg))
+            .chain(log_indexer.map_error(anyhow::Error::msg));
+
+        run_indexer(
+            &mut indexer,
+            NeardataServerProvider::mainnet(),
+            IndexerOptions {
+                range: if std::env::args().len() > 1 {
+                    // For debugging
+                    let msg = "Usage: `all-indexers` or `all-indexers [start-block] [end-block]`";
+                    BlockIterator::iterator(
+                        std::env::args()
+                            .nth(1)
+                            .expect(msg)
+                            .replace(['_', ',', ' ', '.'], "")
+                            .parse()
+                            .expect(msg)
+                            ..=std::env::args()
+                                .nth(2)
+                                .expect(msg)
+                                .replace(['_', ',', ' ', '.'], "")
+                                .parse()
+                                .expect(msg),
+                    )
+                } else {
+                    BlockIterator::AutoContinue(AutoContinue::default())
+                },
+                preprocess_transactions: Some(PreprocessTransactionsSettings {
+                    prefetch_blocks: if cfg!(debug_assertions) { 0 } else { 100 },
+                    postfetch_blocks: if cfg!(debug_assertions) { 0 } else { 100 },
+                }),
+                ..Default::default()
+            },
+        )
+        .await
+        .expect("Indexer run failed");
+    }
 }
